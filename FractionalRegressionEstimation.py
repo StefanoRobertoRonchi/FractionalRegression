285e3245b7bc4357a7ebb8d568008843fe667c88
Stefano Roberto Ronchi <sterobironchi5@gmail.com>
2026-03-01 22:38:00 +0100

    solving some bugs

--- FractionalRegressionEstimation.py
+++ FractionalRegressionEstimation.py
@@ -28,15 +28,15 @@ class FracRegressionEstimation:
         # Implement the fitting procedure for fractional regression
         # ======= Step 0 - Defiinition of Log Likelihood function =======
         X = sparse.csr_matrix(np.asmatrix(X))    
-        y = np.asarray(y)
-        lambda logl(X,y,Beta): np.sum(y * (X @ Beta) - np.logaddexp(0,X @ Beta))
-        lambda l_grad(X,y,Beta): X.T @ (y - 1/(1+np.exp(-X @ Beta))) # Convex Function
+        y = np.asarray(y).reshape(-1, 1)
+        logl = lambda X,y,Beta: np.sum(y * (X @ Beta) - np.logaddexp(0,X @ Beta))
+        l_grad = lambda X,y,Beta: X.T @ (y - 1/(1+np.exp(-X @ Beta))) # Convex Function
         tol = 1e-5
         max_iter = 1000
         # Starting values for the optimization
         D_prev = np.eye(X.shape[1]) # Identity matrix as initial Hessian approximation
         Beta_prev = np.zeros(X.shape[1]).reshape(-1,1) 
-        g_prev = l_grad(X,y,Beta).reshape(-1,1)
+        g_prev = l_grad(X,y,Beta_prev).reshape(-1,1)
         # ======= Step 1 - Estimation of the parameters using BFGS optimization =======
         it = 0
         while np.sqrt(g_prev.T @ g_prev) > tol and it < max_iter:
@@ -53,15 +53,15 @@ class FracRegressionEstimation:
             s = Beta - Beta_prev
             yk = g - g_prev
 
-            ys = float(yk.T @ p)          # y^T s
+            ys = float(yk.T @ s)          # y^T s
             if ys <= 1e-12:
                 D = D_prev               # skip update if curvature condition is not satisfied
             else:
                 Dy  = D_prev @ yk
                 yDy = float(yk.T @ Dy)
                 D = (D_prev
-                    + (1.0 + yDy/ys) * (p @ p.T) / ys
-                    - (Dy @ p.T + p @ Dy.T) / ys
+                    + (1.0 + yDy/ys) * (s @ s.T) / ys
+                    - (Dy @ s.T + s @ Dy.T) / ys
                 )
             
             # Saving previous steps
@@ -70,17 +70,17 @@ class FracRegressionEstimation:
             D_prev = D.copy()
             it+=1
 
-            # storing final parameters 
-            self.Beta = Beta.copy()
-            self.D = D.copy()
-            # ======= Step 1.3 - Compute Standard Errors =======
-            # Compute the standard errors under robust sandwich estimator formula
-            # Define the scores #
-            u = y - 1/(1+np.exp(-X @ Beta)) # scores
-            U =  np.diag(u*u)
-            # Compute the robust sandwich estimator for standard errors
-            self.VarBeta = self.D @ (X.T @ U @ X) @ self.D
-            self.SEBeta = np.sqrt(np.diag(self.VarBeta)).reshape(-1,1)
+        # storing final parameters 
+        self.Beta = Beta.copy()
+        self.D = D.copy()
+        # ======= Step 1.3 - Compute Standard Errors =======
+        # Compute the standard errors under robust sandwich estimator formula
+        # Define the scores #
+        u = y - 1/(1+np.exp(-X @ Beta)) # scores
+        U =  X.T @ X.multiply(u**2)
+        # Compute the robust sandwich estimator for standard errors
+        self.VarBeta = self.D @ (X.T @ U @ X) @ self.D
+        self.SEBeta = np.sqrt(np.diag(self.VarBeta)).reshape(-1,1)
 
         pass
 
